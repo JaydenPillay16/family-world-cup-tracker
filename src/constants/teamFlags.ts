fd4939c04dcd748a6747c9496b378c33130e9cff
Jayden Pillay <jaydenpillay46@gmail.com>
2026-06-14 04:27:15 +0200

    Fix team flag mappings

--- src/constants/teamFlags.ts
+++ src/constants/teamFlags.ts
@@ -1,12 +1,72 @@
 export const teamCodes: Record<string, string> = {
   USA: "US",
+  "United States": "US",
+
   Mexico: "MX",
   "South Africa": "ZA",
   "South Korea": "KR",
   Czechia: "CZ",
+
   Canada: "CA",
   "Bosnia & Herzegovina": "BA",
+  "Bosnia-Herzegovina": "BA",
+
   Paraguay: "PY",
+  Qatar: "QA",
+  Switzerland: "CH",
+
+  Brazil: "BR",
+  Morocco: "MA",
+  Haiti: "HT",
+  Scotland: "GB-SCT",
+
+  Australia: "AU",
+  Turkey: "TR",
+  Türkiye: "TR",
+
+  Germany: "DE",
+  Curaçao: "CW",
+  Curacao: "CW",
+  "Ivory Coast": "CI",
+  "Côte d'Ivoire": "CI",
+  Ecuador: "EC",
+
+  Netherlands: "NL",
+  Japan: "JP",
+  Sweden: "SE",
+  Tunisia: "TN",
+
+  Spain: "ES",
+  "Cape Verde": "CV",
+  "Cape Verde Islands": "CV",
+  "Saudi Arabia": "SA",
+  Uruguay: "UY",
+
+  Belgium: "BE",
+  Egypt: "EG",
+  Iran: "IR",
+  "New Zealand": "NZ",
+
+  France: "FR",
+  Senegal: "SN",
+  Iraq: "IQ",
+  Norway: "NO",
+
+  Argentina: "AR",
+  Algeria: "DZ",
+  Austria: "AT",
+  Jordan: "JO",
+
+  Portugal: "PT",
+  "DR Congo": "CD",
+  "Congo DR": "CD",
+  Uzbekistan: "UZ",
+  Colombia: "CO",
+
+  England: "GB-ENG",
+  Croatia: "HR",
+  Ghana: "GH",
+  Panama: "PA",
 };
 
 export function getTeamCode(team: string) {
